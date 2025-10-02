e0d532a9b6c96079bf975fa3f7103dbb43216d56
Youssef-Hesham123 <youssefhesham1route@gmail.com>
2025-10-03 00:33:13 +0300

    up44

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,18 +5,18 @@ import { NextResponse, NextRequest } from 'next/server';
 export async function middleware(request: NextRequest) {
     // now has a function name getToken return token is obj take req : request
 
-    // const token = await getToken({ req: request });
+    const token = await getToken({ req: request });
 
-    // if (token?.token) {
-    //     return NextResponse.next(); // if has a token next
-    // } else {
-    //     const loginUrl = new URL('/auth/login', request.url);
-    //     loginUrl.searchParams.set(
-    //         'callbackUrl',
-    //         request.nextUrl.pathname + request.nextUrl.search
-    //     );
-    //     return NextResponse.redirect(loginUrl);
-    // }
+    if (token?.token) {
+        return NextResponse.next(); // if has a token next
+    } else {
+        const loginUrl = new URL('/auth/login', request.url);
+        loginUrl.searchParams.set(
+            'callbackUrl',
+            request.nextUrl.pathname + request.nextUrl.search
+        );
+        return NextResponse.redirect(loginUrl);
+    }
 }
 export const config = {
     matcher: ['/cart/:path*', '/wishlist/:path*', '/products/:path*', '/brands/:path*', '/categories/:path*', '/'],
