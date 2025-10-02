41f281b9a57e3508c1b1a975ada6e00954f71563
Youssef-Hesham123 <youssefhesham1route@gmail.com>
2025-10-03 02:35:46 +0300

    up99

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,10 @@ import { NextResponse, NextRequest } from 'next/server';
 export async function middleware(request: NextRequest) {
     // now has a function name getToken return token is obj take req : request
 
-    const token = await getToken({ req: request });
+    const token = request.cookies.get("next-auth.session-token")?.value ||
+    request.cookies.get("__Secure-next-auth.session-token")?.value;;
 
-    if (token?.token) {
+    if (token) {
         return NextResponse.next(); // if has a token next
     } else {
         const loginUrl = new URL('/auth/login', request.url);
